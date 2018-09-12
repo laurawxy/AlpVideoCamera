ea19da2308d776336667da116fc3d9cfd6f2321b
alpface <seyooe@gmail.com>
2018-09-12 11:15:18 +0800

    Initial commit

---
+++ AlpVideoCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlpVideoCamera
+//
+//  Created by xiaoyuan on 2018/9/12.
+//  Copyright © 2018 xiaoyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AlpVideoCamera/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AlpVideoCamera
+//
+//  Created by xiaoyuan on 2018/9/12.
+//  Copyright © 2018 xiaoyuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
